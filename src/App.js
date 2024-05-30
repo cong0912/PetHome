d77a5e3705d392337614f2c1917334ca948e4ca4
vietlucoder <vietleruoi@gmail.com>
2024-05-30 23:36:29 +0700

    set up

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import ProductCard from "./Atomic Components/Molescule/ProductCards/ProductCard"
 import BasicSelect from "./Atomic Components/Atom/select/BasicSelect";
 import Header from "./Organisms/Header/Header";
 import Footer from "./Organisms/Footer/Footer";
+import Register from "./Atomic Components/Register";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
   return (
-    <ProductCard
-    status={"con hang"}
-    content={"do an cho meo 15kg"}
-    forType={"cho meo"}
-    price={"15.000d"}
-    />
+    // <ProductCard
+    //   status={"con hang"}
+    //   content={"do an cho meo 15kg"}
+    //   forType={"cho meo"}
+    //   price={"15.000d"}
+    // />
+    <Router>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        {/* <Route path="/login" element={<Login />} /> */}
+      </Routes>
+    </Router>
   );
 }
 
