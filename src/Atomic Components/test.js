205a292d031b4c3e19ff6d0aca1d5166387937b2
vietlucoder <vietleruoi@gmail.com>
2024-05-31 11:43:03 +0700

    ok

--- src/Atomic Components/test.js
+++ src/Atomic Components/test.js
@@ -4,7 +4,7 @@ export default function Test() {
   return (
     <div>
       <h1 className="p-10 text-center">test ne ok</h1>
-      <span className="h-28 bg-green-600">tesst</span>
+      <span className="h-28 bg-green-600">tesst ne</span>
     </div>
   );
 }
