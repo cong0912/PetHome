d49520fe001346090feca7418505a2ecdbca2235
vietlucoder <vietleruoi@gmail.com>
2024-05-31 11:35:49 +0700

    m

--- src/Atomic Components/test.js
+++ src/Atomic Components/test.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Test() {
   return (
     <div>
-      <h1 className="p-10 text-center">test ne</h1>
+      <h1 className="p-10 text-center">test ne ok</h1>
       <span className="h-28 bg-green-600">tesst</span>
     </div>
   );
