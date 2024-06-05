d1e90ef17c05619e6178ec7907aeec2c4c14cc14
vietlucoder <vietleruoi@gmail.com>
2024-06-05 13:46:49 +0700

    404 page

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,8 @@ import VerifiOTP from "Components/Molescule/VerifyOtp";
 import Register from "Components/Molescule/RegisterForm/index.jsx";
 import GioiThieu from "Components/Molescule/GioiThieu/GioiThieu";
 import Contact from "Components/Molescule/Contact/Contact";
-
+import ErrorPage from "Components/Molescule/ErrorPage/ErrorPage";
+import Test from "Components/Molescule/test";
 // const role = localStorage.getItem("role");
 const publicRoute = [
   { path: "/register", component: Register, layout: null },
@@ -12,6 +13,8 @@ const publicRoute = [
   { path: "/login", component: LoginForm, layout: null },
   { path: "/verify", component: VerifiOTP, layout: null },
   { path: "/contact", component: Contact, layout: DefaultLayout },
+  { path: "/", component: Test, layout: DefaultLayout },
+  { path: "*", component: ErrorPage, layout: null },
 ];
 
 const privateRoute = [];
