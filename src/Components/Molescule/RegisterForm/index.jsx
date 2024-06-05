1358bab1af75cdcc779e44c33bd89670ae054d3d
vietlucoder <vietleruoi@gmail.com>
2024-06-05 14:19:09 +0700

    404 page

--- src/Components/Molescule/RegisterForm/index.jsx
+++ src/Components/Molescule/RegisterForm/index.jsx
@@ -4,8 +4,8 @@ import UserAuthForm from "./user-auth-form.jsx";
 export default function Register() {
   return (
     <AuthenticationLayout>
-      <div className="lg:p-8 relative">
-        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+      <div className="lg:p-8 relative ">
+        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px] ">
           <div className="flex flex-col space-y-2 text-center items-center gap-2">
             <h1 className="text-2xl font-semibold tracking-tight">PET HOME</h1>
             <p className="text-sm text-muted-foreground">
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,8 @@ import Register from "Components/Molescule/RegisterForm/index.jsx";
 import GioiThieu from "Components/Molescule/GioiThieu/GioiThieu";
 import Contact from "Components/Molescule/Contact/Contact";
 import ProductPage from "Components/Molescule/ProductPage/Product";
-
+import ErrorPage from "Components/Molescule/ErrorPage/ErrorPage";
+import Test from "Components/Molescule/test";
 // const role = localStorage.getItem("role");
 const publicRoute = [
   { path: "/register", component: Register, layout: null },
@@ -14,6 +15,8 @@ const publicRoute = [
   { path: "/verify", component: VerifiOTP, layout: null },
   { path: "/contact", component: Contact, layout: DefaultLayout },
   { path: "/product", component: ProductPage, layout: DefaultLayout },
+  { path: "/", component: Test, layout: DefaultLayout },
+  { path: "*", component: ErrorPage, layout: null },
 ];
 
 const privateRoute = [];
